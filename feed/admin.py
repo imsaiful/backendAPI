f11dd1b81b0819cda7cb8e80374c880ba1706575
imsaiful <saifulcseng@gmail.com>
2019-03-28 18:07:30 +0530

    add in model

--- feed/admin.py
+++ feed/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Anchor
+from .models import Anchor,News_Channel,Count,Review
 
 admin.site.register(Anchor)
+admin.site.register(News_Channel)
+admin.site.register(Count)
+admin.site.register(Review)
+
+
 
 # Register your models here.
