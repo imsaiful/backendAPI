b40a24ac68def40df06c6fc64ac5fcdeb1423955
imsaiful <saifulcseng@gmail.com>
2019-04-09 17:47:24 +0530

    change rds

--- fourthpillar/settings.py
+++ fourthpillar/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'fourthpillar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
