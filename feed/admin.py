f19bde68cf1d98e865d5238710c72b6a38f7fb23
imsaiful <saifulcseng@gmail.com>
2019-04-09 18:28:06 +0530

    add model in admin

--- feed/admin.py
+++ feed/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-from .models import Anchor,News_Channel,Count,Review
+from .models import (Anchor,News_Channel,Count,Review,
+    Republic,Ndtv,News18,Indiatoday,Zeenews,
+    Thehindu,Hindustan,IndexTop10,Dna,
+    Indianexpress,Oneindia)
 
 admin.site.register(Anchor)
 admin.site.register(News_Channel)
 admin.site.register(Count)
 admin.site.register(Review)
+admin.site.register(Republic)
+admin.site.register(Ndtv)
+admin.site.register(Zeenews)
+admin.site.register(Thehindu)
+admin.site.register(News18)
+admin.site.register(Hindustan)
+admin.site.register(Indianexpress)
+admin.site.register(Oneindia)
+admin.site.register(Indiatoday)
+admin.site.register(Dna)
+admin.site.register(IndexTop10)
+
+
 
 
 
